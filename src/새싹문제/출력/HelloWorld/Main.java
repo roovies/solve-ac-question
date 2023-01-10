112b565a2968409e9f10e0ca9aed126009085928
이지환 <roovies55@gmail.com>
2023-01-10 16:18:55 +0900

    1. 새싹문제 - 출력

---
+++ src/새싹문제/출력/HelloWorld/Main.java
@@ -0,0 +1,11 @@
+package 새싹문제.출력.HelloWorld;
+
+/*
+ 	[2557] Hello World
+ 	Hello World!를 출력하시오.
+*/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
