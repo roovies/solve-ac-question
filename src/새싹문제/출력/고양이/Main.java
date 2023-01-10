0a4691d39bd9a9e23617d088ec9bcbf5b840ca56
이지환 <roovies55@gmail.com>
2023-01-10 16:30:55 +0900

    1. 새싹문제 - 출력

---
+++ src/새싹문제/출력/고양이/Main.java
@@ -0,0 +1,10 @@
+package 새싹문제.출력.고양이;
+
+public class Main {
+	public static void main(String [] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
---
+++ src/새싹문제/출력/등록/Main.java
@@ -0,0 +1,8 @@
+package 새싹문제.출력.등록;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(2);
+		System.out.println("roovies");
+	}
+}
